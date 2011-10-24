c51f40ef282c3a7ac25f33d8e2ae8c936628de44
sy0725work <sy0725work@gmail.com>
2011-10-24 16:27:40 +0000

    base1.0

---
+++ scripts/myscript.js
@@ -0,0 +1,13 @@
+var mydiv = document.getElementById("imageContent");
+
+$(function() {
+});
+
+function onDesignProcessClick(){
+	mydiv.style.width = '450px'
+	
+};
+function onDesignTeamClick(){
+	mydiv.style.width = '960px'
+	
+};
