d8f58e74be57be6b5d588683d50611bea57be7fa
sy0725work <sy0725work@gmail.com>
2011-11-07 16:25:47 +0000

    use coin-slider

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -5,9 +5,11 @@ $(function() {
 
 function onDesignProcessClick(){
 	mydiv.style.width = '450px'
+	$('#coin-slider').coinslider({ width: 300, delay: 5000 });
 	
 };
 function onDesignTeamClick(){
 	mydiv.style.width = '960px'
+	$('#coin-slider').coinslider({ width: 800, delay: 5000 });
 	
 };
